c545f2dff44657473cbd5c79d8155a1c4301a20d
agus-darmawan <wayanagus.dr@gmail.com>
2025-05-14 19:47:15 +0700

    feat: add cost table

---
+++ database/migrations/1747226647614_create_costs_table.ts
@@ -0,0 +1,25 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'costs'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('name').notNullable()
+      table.integer('price').notNullable()
+      table
+        .integer('responsible_person_id')
+        .unsigned()
+        .references('id')
+        .inTable('users')
+        .onDelete('CASCADE')
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
